526c0af662a0f5f4aa7e0c46b813907fb15a3c72
khunrebirth <khunrebirth@gmail.com>
2017-12-12 21:47:09 +0700

    'init'

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <new-quote @quoteAdded="newQuote"></new-quote>
+        <quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue'
+    import NewQuote from './components/NewQuote.vue'
+    import QuoteGrid from './components/QuoteGrid.vue'
+
+    export default {
+        components: {
+            appHeader: Header,
+            NewQuote,
+            QuoteGrid
+        },
+        data () {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote (quote) {
+                if (this.quotes.length >= this.maxQuotes)
+                    return alert('Please delete Quotes first!')
+
+                this.quotes.push(quote)
+            },
+            deleteQuote (index) {
+                this.quotes.splice(index, 1)
+            }
+        }
+    }
+</script>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+            <label>Quote</label>
+            <textarea name="" id="" cols="30" rows="10" class="form-control" v-model="quote"></textarea>
+        </div>
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+            <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            createNew () {
+                this.$emit('quoteAdded', this.quote)
+                this.quote = ''
+            }
+        }
+    }
+</script>
+
+<style>
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
+</style>
